75a4f979fa0c747d7c39790b034f26536219d69a
zqr <zhouqingrong_zqr@163.com>
2021-03-10 23:09:50 +0800

    layoutTest

--- src/view/layout/index.vue
+++ src/view/layout/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div class="layoutConatiner" style="height：1000px">
     <el-container>
       <el-header>Header</el-header>
       <el-container>
-        <el-aside width="200px" height="100%">
-          <el-tabs tab-position="left" style="height: 100%">
+        <el-aside width="200px">
+          <el-tabs tab-position="left" style="height: 800px">
             <el-tab-pane label="用户管理">用户管理</el-tab-pane>
             <el-tab-pane label="配置管理">配置管理</el-tab-pane>
             <el-tab-pane label="角色管理">角色管理</el-tab-pane>
@@ -19,6 +19,45 @@
 
 <script>
 export default {
-  name: 'Layout'
-}
+  name: "Layout",
+};
 </script>
+<style scoped>
+#layoutConatiner {
+  height: 100%;
+}
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
