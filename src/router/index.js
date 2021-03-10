8f778999599a4d96f365f669a6555d2410867481
bqxtt <1486126243@qq.com>
2021-03-10 22:35:11 +0800

    eslint disable

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Layout from '@/view/Layout'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [{
-    path: '/',
-    name: 'Layout',
-    component: Layout
-  }]
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
 })
