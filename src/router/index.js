375fa7e33eb6e78b7f0f3ccecf8f154242ac2729
zhouqingrong <1085620784@qq.com>
2021-03-17 19:27:55 +0800

    anchor

--- src/router/index.js
+++ src/router/index.js
@@ -7,41 +7,48 @@ import Contest from '@/view/contest'
 import Status from '@/view/status'
 import ProblemInfo from '@/view/problem_info'
 import ContestInfo from '@/view/contest_info'
+import Test from '@/view/test.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [{
-    path: '/',
-    name: 'Layout',
-    component: Layout,
-    redirect: '/home',
-    children: [{
-      path: 'home',
-      name: 'home',
-      component: Home,
-    }, {
-      path: 'problem',
-      name: 'problem',
-      component: Problem,
-    }, {
-      path: '/problem/:problem_id',
-      name: 'problemInfo',
-      component: ProblemInfo,
-      props: true
-    }, {
-      path: 'contest',
-      name: 'contest',
-      component: Contest,
-    }, {
-      path: '/contest/:contest_id',
-      name: 'contestInfo',
-      component: ContestInfo,
-      props: true
-    }, {
-      path: 'status',
-      name: 'status',
-      component: Status,
+  routes: [
+    {
+      path: '/test',
+      name: 'Test',
+      component: Test,
+    },
+    {
+      path: '/',
+      name: 'Layout',
+      component: Layout,
+      redirect: '/home',
+      children: [{
+        path: 'home',
+        name: 'home',
+        component: Home,
+      }, {
+        path: 'problem',
+        name: 'problem',
+        component: Problem,
+      }, {
+        path: '/problem/:problem_id',
+        name: 'problemInfo',
+        component: ProblemInfo,
+        props: true
+      }, {
+        path: 'contest',
+        name: 'contest',
+        component: Contest,
+      }, {
+        path: '/contest/:contest_id',
+        name: 'contestInfo',
+        component: ContestInfo,
+        props: true
+      }, {
+        path: 'status',
+        name: 'status',
+        component: Status,
+      }]
     }]
-  }]
 })
---
+++ src/view/test.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="testContainer">
+    <!-- 跳转锚点  -->
+    <a href="javascript:void(0)" @click="goAnchor('production')">
+      作品 a标签
+    </a>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <!-- // 跳转目的地 -->
+    <div class="production-box" id="production">
+      <span class="item-name">作品 跳转目的地</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "TestAdmin",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {
+    goAnchor(id) {
+      var anchor = document.getElementById(id);
+      console.log("test0:anchor.offsetTop", anchor.offsetTop);
+      // chrome
+      document.body.scrollTop = anchor.offsetTop;
+      console.log("test1:document.body.scrollTop", document.body.scrollTop);
+      // firefox
+      document.documentElement.scrollTop = anchor.offsetTop;
+      console.log(
+        "test2: document.documentElement.scrollTop",
+        document.documentElement.scrollTop
+      );
+      // safari
+      window.pageYOffset = anchor.offsetTop;
+      console.log("test3:window.pageYOffset", window.pageYOffset);
+    },
+  },
+};
+</script>
+<style scoped>
+.item {
+  width: 60%;
+  height: 50px;
+  margin: 20px 0;
+  background-color: pink;
+}
+</style>
